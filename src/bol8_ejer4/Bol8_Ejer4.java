c7eaccba232498d35aac8eed5e7793d9bd484e09
Ero <ero.sobrino.dorado@gmail.com>
2018-06-07 11:06:07 +0200

    Primer commit

---
+++ src/bol8_ejer4/Bol8_Ejer4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol8_ejer4;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ero
+ */
+class Principal extends JFrame{
+    
+}
+
+public class Bol8_Ejer4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal p=new Principal();
+        p.setSize(500,500);
+        p.setLocationRelativeTo(null);
+        p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        p.setVisible(true);
+    }
+    
+}
